bde86c9c70ac45eb8e24ee9765cd812ba0539cc5
ylabate <12196@holbertonstudents.com>
2026-06-09 10:42:25 +0200

    test(bruno): Add workspace and API collection

--- levelup/backend/app/__init__.py
+++ levelup/backend/app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
+jwt = JWTManager()
 
 
 def create_app():
